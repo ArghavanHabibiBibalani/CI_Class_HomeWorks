1ff66cda4ad2b683ae92f12a7eed9c12dd641b24
arghavan <93511586+ArghavanHabibiBibalani@users.noreply.github.com>
2024-01-06 04:07:54 -0800

    XO pattern classification with Hebb

---
+++ XO with Hebb/XO/XO/Hebb.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XO
+{
+    public class Hebb
+    {
+        private readonly int[] _weights = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int[] _deltaWeights = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        
+        private readonly List<Data> _separatedData;
+        private readonly int[] _testingData;
+        
+        private bool _processCompleted = false;
+
+        private int _deltaBias = 0;
+        private int _bias = 0;
+        public bool ProcessCompleted => _processCompleted;
+
+        public Hebb(List<Data> separatedData, int[] testingData)
+        {
+            _separatedData = separatedData;
+            _testingData = testingData;
+        }
+
+        public void Training(List<Data> separatedData)
+        {
+            foreach (Data data in separatedData)
+            {
+                Array.Clear(_deltaWeights, 0, _deltaWeights.Length); // Clear delta weights for each data point
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        var propertyValue = (int)data.GetType().GetProperty($"x{i + 1}").GetValue(data);
+                        _deltaWeights[j] += propertyValue * data.GetX26();
+                        _deltaBias = data.GetX26();
+                    }
+                     
+                }
+
+                for (int j = 0; j < 25; j++)
+                {
+                    _weights[j] += _deltaWeights[j];
+                }
+                _bias += _deltaBias;
+            }
+
+            _processCompleted = true;
+        }
+
+
+
+
+        public string Testing(int[] testingData)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 25; i++)
+            {
+                result += testingData[i] * _weights[i];
+            }
+
+            result += _bias;
+
+            return result > 0 ? "x" : "o";
+        }
+
+    }
+}
---
+++ XO with Hebb/XO/XO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            List<Data> sapratedData = new List<Data>();
+            int[] testingData = new int[25];
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(sapratedData, testingData));
+
+
+        }
+    }
+}
+
+
